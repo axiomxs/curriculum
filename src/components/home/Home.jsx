6cffaf6bd993d18d2cc0f52b793d79111b68a473
lingjue <3266764066@qq.com>
2023-12-30 14:45:02 +0800

    更新主页距离

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -9,7 +9,7 @@ import { Link } from "react-router-dom";
 
 export const Home = () => {
   return (
-    <div className="diff aspect-[16/9] w-full h-screen">
+    <div className="diff aspect-[16/9] w-full h-fit">
       <div className="diff-item-1">
         <div className="bg-primary text-primary-content text-9xl font-black grid place-content-center">
           LingJue
